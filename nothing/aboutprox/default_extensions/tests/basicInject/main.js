a0a0aafe6381175c374d10720792c53fcd5bd6ca
Kilo <145521434+kkilobyte@users.noreply.github.com>
2023-10-23 12:33:21 -0700

    Delete nothing/aboutprox directory

--- nothing/aboutprox/default_extensions/tests/basicInject/main.js
+++
@@ -1,6 +0,0 @@
-console.log("Hello from aboutbrowser!")
-
-console.log("chrome.runtime:")
-console.log("chrome.runtime.id:", chrome.runtime.id);
-console.log("chrome.runtime.getManifest:", chrome.runtime.getManifest());
-console.log("chrome.runtime.getURL:", chrome.runtime.getURL("/owo"));
--- nothing/aboutprox/default_extensions/tests/basicPlugin/main.js
+++
@@ -1,7 +0,0 @@
-()=>{
-  const eventel = document.querySelector(".aboutbrowser-event-el#aboutbrowser-event-el");
-  eventel.addEventListener("aboutbrowser-contextmenu", (/*e*/)=>{
-    // e.preventDefault();
-    console.log("context menu");
-  });
-}
